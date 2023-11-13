57c75ecce42bbf30efba646ddfd1d917fdd3dafd
Tyler Palesano <tpalesano@interworks.com>
2023-11-13 11:31:55 -0600

    Tidying up uncommitted files.

--- EntityFramework.Utilities/EntityFramework.Utilities/Properties/AssemblyInfo.cs
+++ EntityFramework.Utilities/EntityFramework.Utilities/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("EntityFramework.Utilities")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Fork of https://github.com/MikaelEliasson/EntityFramework.Utilities")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("EntityFramework.Utilities")]
